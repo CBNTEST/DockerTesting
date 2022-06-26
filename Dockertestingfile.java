66f282bef52796c22eb24ce837587262ed38769a
CBNTEST <107784581+CBNTEST@users.noreply.github.com>
2022-06-26 14:31:48 +0530

    loadinglatest

--- Dockertestingfile.java
+++ Dockertestingfile.java
@@ -1,11 +1,11 @@
 public class Dockertestingfile {
-
  public static void main(String[] args)  //static method
  {
   System.out.println("Static method");
   System.out.println("Static method");
   System.out.println("Static method");
+  System.out.println("Static method latest one");
+  System.out.println("Static method latest one");
+
  }
-
-
 }
